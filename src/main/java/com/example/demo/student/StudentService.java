2f07f97e2e5cf425a35745871434b1fd963cbfb0
KidusMT <kidusmamuye@gmail.com>
2021-03-23 07:03:16 -0500

    PUT http method implemented by even checking for existed email before updating and throwing exception

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,35 +3,55 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class StudentService {
 
-    private StudentRepository repository;
+    private final StudentRepository repository;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository){
+    public StudentService(StudentRepository studentRepository) {
         repository = studentRepository;
     }
 
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return repository.findAll();
     }
 
-    public void registerStudent(Student student){
+    public void registerStudent(Student student) {
         Optional<Student> studentOptional = repository.findStudentByEmail(student.getEmail());
-        if(studentOptional.isPresent()){
+        if (studentOptional.isPresent()) {
             throw new IllegalStateException("user existed");
         }
         repository.save(student);
     }
 
     public void deleteStudent(Long studentId) {
-        if(repository.findById(studentId).isEmpty()){
-            throw new IllegalStateException("user with id:"+studentId+" doesn't exist");
+        if (repository.findById(studentId).isEmpty()) {
+            throw new IllegalStateException("user with id:" + studentId + " doesn't exist");
         }
         repository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = repository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student doesn't exist."));
+        if (name != null && name.length() > 0 &&
+                !name.equals(student.getName())) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0) {
+            Optional<Student> studentOptional = repository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email already exist.");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
